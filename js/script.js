abf5aa79bf22b301bf22b4cce4d2f61ff85ce1be
adityasukmaa <adityasukma67@gmail.com>
2023-06-30 22:30:37 +0700

    memperbaharui js/script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@ window.addEventListener ("scroll", function() {
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header ul li a');
+
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
@@ -15,3 +18,19 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
 }
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+ 
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header ul li a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
